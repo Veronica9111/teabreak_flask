eea8d904223eedbc630b235ed4a47a60c0968b7f
Veronica <anjjellia@gmail.com>
2013-05-06 20:34:05 +0800

    Add register page

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
 from flask import *
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-def hello():
+def index():
 	return render_template("index.html")
+
+@app.route("/register")
+def register():
+	return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
