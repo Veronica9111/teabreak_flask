995e3e6fc33fc9812af0994e0e3ef9fe93187f0b
Veronica <anjjellia@gmail.com>
2013-05-11 23:22:10 +0800

    Add register actions

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import *
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,9 +8,17 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		#user = User(form.username.data, form.email.data, form.password.data)
+		#userDB.add(user)
+		user_data = request.values
+		return render_template('main.html', nickname=user_data['nickname'])
 	return render_template("register.html")
 
+@app.route("/main", methods=['GET'])
+def get_main_page():
+	return render_template('main.html', nickname)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
